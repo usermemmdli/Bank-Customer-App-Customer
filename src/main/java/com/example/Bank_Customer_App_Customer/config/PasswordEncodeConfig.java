94e526f9d6b8b86b6fe15bfa30db6b31cbe48d14
usermemmdli <mustafamemmedli005@mail.ru>
2024-11-19 00:34:10 +0400

    commit

---
+++ src/main/java/com/example/Bank_Customer_App_Customer/config/PasswordEncodeConfig.java
@@ -0,0 +1,14 @@
+package com.example.Bank_Customer_App_Customer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordEncodeConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
